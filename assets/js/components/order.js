f0136c81ff7ac8422e14bb3ebf2288850ada4da0
Michał Białkowski <michal@bialkowski.net.pl>
2020-05-18 23:26:27 +0200

    Update

--- assets/js/components/order.js
+++
@@ -1,4 +0,0 @@
-$('.order--product').on('click', function() {
-	$('#modal__product').modal('hide');
-	$('#modal__order').modal('show');
-});
--- assets/js/components/sticky.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function() {
-	$("#sticker").sticky(
-		{
-			bottomSpacing: 0
-		}
-	);
-});
